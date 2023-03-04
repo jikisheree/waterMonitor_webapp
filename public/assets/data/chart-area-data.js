e22d253721424438ecf2a7c60b56d081f3966491
min23asdw <76154445+min23asdw@users.noreply.github.com>
2023-03-04 18:08:05 +0700

    add:fetch realtimeDB firebase & remove unnecessary file

--- public/assets/data/chart-area-data.js
+++ public/assets/data/chart-area-data.js
@@ -2,12 +2,22 @@
 Chart.defaults.global.defaultFontFamily = '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
 Chart.defaults.global.defaultFontColor = '#292b2c';
 
+ 
+
+
 // Area Chart Example
 var ctx = document.getElementById("myAreaChart");
+
+ 
+var data_date = ["Mar 1", "Mar F", "Mar 3", "Mar 4", "Mar 5", "Mar 6", "Mar 7", "Mar 82", "Mar 9", "Mar 10", "Mar 11", "Mar 12", "Mar A","Mar V", "Mar 12", "Mar 13","Mar 11", "Mar 12", "Mar 13"];
+var data_value = [10000, 30162, 26263, 18394, 18287, 28682, 31274, 33259, 25849, 24159, 32651, 31984, 38451,32651, 31984, 38451,32651, 31984, 38451];
+
+
+
 var myLineChart = new Chart(ctx, {
   type: 'line',
   data: {
-    labels: ["Mar 1", "Mar 2", "Mar 3", "Mar 4", "Mar 5", "Mar 6", "Mar 7", "Mar 8", "Mar 9", "Mar 10", "Mar 11", "Mar 12", "Mar 13"],
+    labels: data_date,
     datasets: [{
       label: "Sessions",
       lineTension: 0.3,
@@ -20,10 +30,14 @@ var myLineChart = new Chart(ctx, {
       pointHoverBackgroundColor: "rgba(2,117,216,1)",
       pointHitRadius: 50,
       pointBorderWidth: 2,
-      data: [10000, 30162, 26263, 18394, 18287, 28682, 31274, 33259, 25849, 24159, 32651, 31984, 38451],
+      data: data_value,
     }],
   },
   options: {
+      responsive: true,
+      maintainAspectRatio: false,
+      scroll: true,
+      tick: 1,
     scales: {
       xAxes: [{
         time: {
@@ -33,14 +47,14 @@ var myLineChart = new Chart(ctx, {
           display: false
         },
         ticks: {
-          maxTicksLimit: 7
+          maxTicksLimit: 15
         }
       }],
       yAxes: [{
         ticks: {
           min: 0,
           max: 40000,
-          maxTicksLimit: 5
+          maxTicksLimit: 25
         },
         gridLines: {
           color: "rgba(0, 0, 0, .125)",
@@ -52,3 +66,5 @@ var myLineChart = new Chart(ctx, {
     }
   }
 });
+
+
